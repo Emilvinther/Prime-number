7cb3e0c0e6f8b7d40a85316a58673e260d09e889
Emil Vinther Lorentzen <emil380e@zbc.dk>
2022-03-22 11:38:08 +0100

    Add project files.

---
+++ Prime number/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Tast et nummer");
+int input = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < input; i++)
+{
+    bool IsPrime = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            IsPrime = false;
+            
+        }
+    }
+
+    if (IsPrime == true)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(i);
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine(i);
+    }
+}
+
+Console.ReadKey();
